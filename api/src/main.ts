4f963f3a151613cf1070c185f5be325ac915f242
Bohdan Hrushetskyi <bogdan1grushetsky@gmail.com>
2023-08-21 22:28:07 +0300

    try 3

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,9 @@ import * as morgan from 'morgan';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
@@ -17,7 +19,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors({origin: true, credentials: true});
   await app.listen(3000);
 }
 bootstrap();
